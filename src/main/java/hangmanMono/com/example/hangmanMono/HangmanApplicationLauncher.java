69071f21305dc518e83eaee6c58bfb5d2bfb3a6d
lawcia <lawrencianjume@gmail.com>
2021-03-31 18:30:29 +0100

    create name endpoint

--- src/main/java/hangmanMono/com/example/hangmanMono/HangmanApplicationLauncher.java
+++ src/main/java/hangmanMono/com/example/hangmanMono/HangmanApplicationLauncher.java
@@ -1,11 +1,12 @@
 package hangmanMono.com.example.hangmanMono;
 
+import hangmanMono.com.example.hangmanMono.api.NameController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HangmanApplicationLauncher {
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldController.class, args);
+		SpringApplication.run(NameController.class, args);
 	}
 }
--- src/main/java/hangmanMono/com/example/hangmanMono/api/NameController.java
+++ src/main/java/hangmanMono/com/example/hangmanMono/api/NameController.java
@@ -1,18 +1,20 @@
-package hangmanMono.com.example.hangmanMono;
+package hangmanMono.com.example.hangmanMono.api;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.UUID;
+
 @Controller
 @EnableAutoConfiguration
-public class HelloWorldController {
+public class NameController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/name")
     @ResponseBody
-    public String sayHello() {
-        // the application is running on http://localhost:8080/hello
-        return "Hello World!!!";
+    public UUID getID() {
+        // the application is running on http://localhost:8080/name
+        return UUID.randomUUID();
     }
 }
