79b4a8373dce0215eeda1922ab63f4d6017a4a87
Joanna Czerwinska <joczerw@gmail.com>
2021-04-07 18:45:58 +0100

    Added a Player class

---
+++ src/main/java/hangmanMono/com/example/hangmanMono/api/Player.java
@@ -0,0 +1,20 @@
+package hangmanMono.com.example.hangmanMono.api;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Player {
+    @Id
+    @SequenceGenerator(
+            name = "player_sequence",
+            sequenceName = "player_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "player_sequence"
+    )
+    private String name;
+    private Long id;
+}
