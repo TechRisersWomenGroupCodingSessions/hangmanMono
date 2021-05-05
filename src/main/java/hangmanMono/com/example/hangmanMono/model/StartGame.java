db928617212e19ff3416dffb4f5f32572153a96a
lawcia <lawrencianjume@gmail.com>
2021-05-05 19:32:09 +0100

    update start game method

--- src/main/java/hangmanMono/com/example/hangmanMono/model/StartGame.java
+++ src/main/java/hangmanMono/com/example/hangmanMono/model/StartGame.java
@@ -2,15 +2,17 @@ package hangmanMono.com.example.hangmanMono.model;
 
 import java.util.UUID;
 
-public class SecretWord {
+public class StartGame {
 
     private final Integer secretWordLength;
     private final UUID gameId;
 
-    public SecretWord(Integer secretWordLength, UUID gameId) {
+    public StartGame(Integer secretWordLength, UUID gameId) {
         this.secretWordLength = secretWordLength;
         this.gameId = gameId;
     }
-
-
+    
+    public UUID getGameId() {
+        return gameId;
+    }
 }
