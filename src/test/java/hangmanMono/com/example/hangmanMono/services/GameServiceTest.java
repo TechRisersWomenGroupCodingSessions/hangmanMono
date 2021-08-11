b5b692b213a53fbc242e5039931c020adf72407e
lawcia <lawrencianjume@gmail.com>
2021-08-11 19:59:07 +0100

    game service unit tests

--- src/test/java/hangmanMono/com/example/hangmanMono/services/GameServiceTest.java
+++ src/test/java/hangmanMono/com/example/hangmanMono/services/GameServiceTest.java
@@ -1,19 +1,41 @@
 package hangmanMono.com.example.hangmanMono.services;
 
+import hangmanMono.com.example.hangmanMono.model.StartGameRequest;
+import hangmanMono.com.example.hangmanMono.model.StartGameResponse;
+import hangmanMono.com.example.hangmanMono.repository.GameRepository;
+import hangmanMono.com.example.hangmanMono.repository.PlayerRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.*;
 
 public class GameServiceTest {
 
+    GameService gameService;
+    SecretWordService mockedSecretWordService;
+    PlayerRepository mockedPlayerRepository;
+
     @BeforeEach
     public void setUp() {
-        // TODO use Mockito
-//        GameService gameService = new GameService();
+        GameRepository mockedGameRepository = mock(GameRepository.class);
+        mockedPlayerRepository = mock(PlayerRepository.class);
+        mockedSecretWordService = mock(SecretWordService.class);
+        gameService = new GameService(mockedGameRepository, mockedPlayerRepository, mockedSecretWordService);
     }
 
     @Test
     public void givenStartTheGameIsCalled_whenPlayerExists_thenItReturnsStartGameResponse() {
+        Long playerId = 1L;
 
+        when(mockedSecretWordService.getSecretWord()).thenReturn("APPLE");
+        when(mockedPlayerRepository.findById(playerId));
+        // TODO: return player optional
+
+        StartGameRequest request = new StartGameRequest(playerId, true);
+        StartGameResponse response = gameService.startTheGame(request);
+
+        assertEquals(5, response.getSecretWordLength());
     }
 }
 
